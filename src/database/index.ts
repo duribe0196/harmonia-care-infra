b5944ad6e750b6c13f236335dec456e92ebdc7a5
duribegl <110846568+duribegl@users.noreply.github.com>
2024-05-23 11:36:33 -0500

    Waiting for mongoatlas project be created

--- src/database/index.ts
+++ src/database/index.ts
@@ -27,10 +27,12 @@ export function createMongoAtlasCluster(args: ICreateMongoAtlasClusterArgs) {
     },
   );
 
+  const projectId = project.id.apply((id) => id);
+
   const ipAccessList = new mongodbatlas.ProjectIpAccessList(
     `${env}-${projectName}-project-ip-access`,
     {
-      projectId: project.id,
+      projectId,
       cidrBlock: "10.0.1.0/24", // Range de IPs VPC/Subnet
       comment: "Access from Lambda VPC",
     },
@@ -38,7 +40,7 @@ export function createMongoAtlasCluster(args: ICreateMongoAtlasClusterArgs) {
 
   const clusterName = `${env}-${projectName}-mongodb-atlas-cluster`;
   const cluster = new mongodbatlas.Cluster(clusterName, {
-    projectId: project.id,
+    projectId,
     providerName: "TENANT",
     backingProviderName: "AWS",
     providerRegionName: region.toUpperCase().replace(/-/g, "_"),
@@ -48,7 +50,7 @@ export function createMongoAtlasCluster(args: ICreateMongoAtlasClusterArgs) {
   const dbUser = new mongodbatlas.DatabaseUser(
     `${env}-${projectName}-mongodb-user`,
     {
-      projectId: project.id,
+      projectId,
       username: `${env}-${projectName}-mongodb-user`,
       password: mongodbPassword,
       roles: [
@@ -62,7 +64,7 @@ export function createMongoAtlasCluster(args: ICreateMongoAtlasClusterArgs) {
   );
 
   return {
-    projectId: project.id,
+    projectId,
     clusterName: cluster.name,
     connectionString: cluster.connectionStrings[0].standardSrv,
     dbUser: dbUser.username,
